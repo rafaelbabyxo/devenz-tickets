8f23e48bf9357960181091f399ddad521d760ca8
hate <rafaelassis@outlook.pt>
2025-03-05 12:26:46 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ORBIT - Auth - Email",
+  description: "Projeto para demonstrar a autenticação com email",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
